f696a50905686f9c6b79b2fede46e6c87bfc4fcf
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-12-21 03:25:02 -0500

    Create Next app and start header

---
+++ app/NavLink.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+type Props = {
+    category: string;
+    isActive: boolean;
+}
+
+function NavLink({category, isActive}: Props) {
+  return (
+    <div></div>
+  )
+}
+
+export default NavLink
