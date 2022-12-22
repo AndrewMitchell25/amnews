ab20af02ee84e8790af167c690d5cd804309061c
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-12-22 15:11:47 -0500

    Article layouts

--- app/Article.tsx
+++ app/Article.tsx
@@ -4,7 +4,8 @@ type Props = {
 
 function Article({article}: Props) {
   return (
-    <article>
+    <article className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-sm hover:scale-105
+    hover:shadow-lg hover:bg-slate-200 transition-all duration-200 ease-out">
         {article.image && (
             <img 
                 src={article.image}
@@ -13,15 +14,15 @@ function Article({article}: Props) {
             />
         )}
 
-        <div>
-            <div>
-                <h2>{article.title}</h2>
+        <div className="flex-1 flex flex-col">
+            <div className="flex-1 flex flex-col p-5">
+                <h2 className="font-bold font-serif">{article.title}</h2>
 
-                <section>
-                    <p>{article.description}</p>
+                <section className="mt-2 flex-1">
+                    <p className="text-xs line-clamp-6">{article.description}</p>
                 </section>
 
-                <footer>
+                <footer className="text-xs text-right ml-auto flex space-x-1 pt-5 italic text-gray-400">
                     <p>{article.source} -</p>
                     <p>{article.published_at}</p>
                 </footer>
